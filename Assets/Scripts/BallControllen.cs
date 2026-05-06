27eb90252c57e08bc0592e52f898b5525d1d4d2c
Andrey <kastolom47@mail.ru>
2026-05-06 22:34:46 +0300

    Появились монетки, они вращаються

--- Assets/Scripts/BallControllen.cs
+++ Assets/Scripts/BallControllen.cs
@@ -11,6 +11,7 @@ public class BallControllen : MonoBehaviour
 
     void Start()
     {
+        Application.targetFrameRate = 120; // устанавливаем частоту кадров
         rb = GetComponent<Rigidbody>(); // получаем Rigidbody при старте
     }
 
